a5a9a2da93ff51971782b4e379281b031c8a236d
Mudasir Fayaz <mudasirfayaz32@gmail.com>
2025-07-08 19:51:40 +0530

    More dynamic alternative by using operator module

--- Calculator App/calculator.py
+++ Calculator App/calculator.py
@@ -1,54 +1,42 @@
-#Calculator App
+import operator
+
+# Mapping of symbols to actual functions
+operations = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv
+}
 
 # Infinite loop to keep the calculator running until the user decides to exit
 while True: 
     try: 
         # Prompt user to input two numbers
+        print(" ") # Add a newline for better readability
         num1 = int(input("Enter first no: ")) 
         num2 = int(input("Enter second no: "))
     except ValueError: 
         # If input is not a valid number, display error and restart loop
         print("Invalid input. Please enter numbers only.")
-        print("") # Add a newline for better readability
         continue # Restart the outer loop
 
     # Inner loop to prompt for operator until a valid one is entered
     while True:
-        
-        operator = input("Enter operation (+ - * /): ") 
+        opr = input("Enter operation (+ - * /): ") 
         print(" ") 
         
-        # Addition
-        if operator == "+":
-            result = num1 + num2
-            print(f"{num1} + {num2} = {result}")
+        if num2 == 0 and opr == "/": # Prevent division by zero
+            print("Division by zero is not allowed")
+            continue # Restart the inner opr loop
+
+        if opr in operations:    
+            result = operations[opr](num1, num2)
+            print(f"{num1} {opr} {num2} = {result}")
             break # Break the operator loop to return to outer loop
-        
-        # Subtraction
-        elif operator == "-":
-            result = num1 - num2
-            print(f"{num1} - {num2} = {result}")
-            break
+        else:
+            print("Invalid operator. Try again.")
+            print
 
-        # Multiplication
-        elif operator == "*":
-            result = num1 * num2
-            print(f"{num1} x {num2} = {result}")
-            break
-        
-        # Division
-        elif operator == "/":
-            if num2 != 0: # Prevent division by zero
-                result = num1 / num2
-                print(f"{num1} / {num2} = {result}")
-                break  
-            else:
-                print("Division by zero is not allowed")
-
-        # Invalid operator input      
-        else: 
-            print("Enter a valid operation") 
-            
     # Ask user if they want to perform another calculation
     print(" ")
     choice = input("Do you want to calculate again Y/N: ").lower()
