2bd2803ede0681f3a198c0b271b6b8a4cfabf1de
Mudasir Fayaz <mudasirfayaz32@gmail.com>
2025-07-11 11:21:21 +0530

    Error handling

--- Password Generator/password.py
+++ Password Generator/password.py
@@ -8,10 +8,16 @@ def generate_password(password_length, use_symbols):
     
     return ''.join(random.choice(material) for _ in range(password_length))
 
-length = int(input("Enter the password length (8-32): "))
-symbols = input("Do you want to include symbols (Y/N): ").lower()
-if symbols == "y":
-    use_symbols = True
+while True:
+    try:
+        length = int(input("Enter the password length (8-32): "))
+        symbols = input("Do you want to include symbols (Y/N): ").lower()
+        if symbols == "y":
+            use_symbols = True
+        break
+    except ValueError:
+        print("Invalid input! Enter password length again (8-32): ")
+        continue
 
 password = generate_password(length, use_symbols)
 print(f"Your password is: {password}")
